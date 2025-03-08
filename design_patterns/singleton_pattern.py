70a957d88037704ca322529dcb4cef6a0916dba7
geeta2790 <geeta.rinkusharma@gmail.com>
2025-03-08 22:09:03 +0530

    Refactored Singleton pattern for better instance management.

---
+++ design_patterns/singleton_pattern.py
@@ -0,0 +1,15 @@
+class Singleton:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+    def some_business_logic(self):
+        return 'Performing some business logic.'
+
+singleton1 = Singleton()
+singleton2 = Singleton()
+
+assert singleton1 is singleton2
+assert singleton1.some_business_logic() == 'Performing some business logic.'
